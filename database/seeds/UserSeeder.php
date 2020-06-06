2731ce20d4bae9e8ee714c0605d1b68d40c65a8d
Nicholas Gratton <nichgratton@gmail.com>
2020-06-06 17:20:24 -0400

    Modif au courriel des utilisateurs par default

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -17,7 +17,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'first_name' => 'Admin',
             'last_name' => 'Istrateur',
-            'email' => 'admin@portager.qc.ca',
+            'email' => 'admin@portager.com',
             'password' => Hash::make('portager'),
             'user_role' => 'Administrateur',
             'api_token' => 'VVGsozaUcRnHdr4qCcyAm9xKVDuGLVo8nxrcWV1NI3i2K5hX6yxhaUWh7FM57TWQ83lAVeJvCaN8m9Vb',
@@ -26,7 +26,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'first_name' => 'Utilisateur',
             'last_name' => 'Lambda',
-            'email' => 'user@portager.qc.ca',
+            'email' => 'user@portager.com',
             'password' => Hash::make('portager'),
             'user_role' => 'Utilisateur',
             'api_token' => 'ho8ubW89aIWH7JOjEVB8XrtD3FWVtnhQQGNsrP3Egp2sxsktHSUIGyB8EHp9sOFiwvXHeALFLbOcsU73',
