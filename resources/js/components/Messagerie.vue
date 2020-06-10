e5ee5eed9c60176d4434fca308b0bbb430f206e4
Yohan <yohan.coderre@outlook.com>
2020-06-09 20:21:20 -0400

    Creation du component de messagerie (work in progress)

---
+++ resources/js/components/Messagerie.vue
@@ -0,0 +1,34 @@
+<template>
+  
+    <div>       
+           
+    </div>
+
+</template>
+
+<script>
+    import Axios from "axios";
+    export default {
+        name: 'Recherche', 
+        data() {
+            return {  
+                data: 0,
+            };
+        
+        },
+        props: {
+
+        },
+        components: {
+
+        },
+        mounted() {
+        //     Axios.get("/api/talent/"+this.nom_fr).then(response => {
+        //     this.talent = response.data;
+        // });
+        },
+        methods: {
+
+        },
+    }
+</script>
