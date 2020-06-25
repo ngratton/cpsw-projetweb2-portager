5bb70c534d8ddf9de4ddb84ba817366a3a2a14ef
Nicholas Gratton <nichgratton@gmail.com>
2020-06-25 15:34:03 -0400

    Correction

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\LengthAwarePaginator;
