d45d34ed77dc4970260f9975d6ac17f57b7ba353
Nicholas Gratton <nichgratton@gmail.com>
2020-07-03 10:04:23 -0400

    Seeder images réelle

--- database/seeds/PlantSeeder.php
+++ database/seeds/PlantSeeder.php
@@ -14,12 +14,18 @@ class PlantSeeder extends Seeder
     {
         $faker = Faker\Factory::create('fr_CA');
 
-        for($i = 0; $i < 50; $i++) {
+        for($i = 0; $i < 200; $i++) {
             $plant = new Plant();
             $plant->type_id = rand($min = 1, $max = 25);
             $plant->description = $faker->paragraph($nbSentences = 2, $variableNbSentences = true);
             $plant->potager_id = rand($min = 1, $max = 9);
             $plant->user_id = $plant->potager_id + 1;
+
+            $randNum = rand($min = 1, $max = 20);
+
+            $plant->photo = '/images/Plants_placeholders/'.$randNum.'.jpg';
+            $plant->photo_mini = '/images/Plants_placeholders/'.$randNum.'.jpg';
+
             $plant->save();
         }
     }
